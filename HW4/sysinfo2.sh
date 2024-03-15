558741bcb8628af387c17c70142152e3555360f6
Anzhalika Tseliak <74267984+anzhalikatseliak@users.noreply.github.com>
2024-03-16 02:27:02 +0300

    Add sysinfo2.sh

---
+++ HW4/sysinfo2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+processes=$(ps -U "user" | wc -1) 
+    echo "User processes:${processes}"
+
+loadaverage=$(uptime | awk '{print $10}') 
+    echo "Load average in 15 minutes:${loadaverage}"
+
+ram=$(free --mega | grep 'Mem:' | awk '{print $4}') 
+    echo "Free memory:${ram}MB"
+
+freeroot=$(df -hT | grep ubuntu | awk '{print $5}' ) 
+    echo "Free space in root:${freeroot}"
